298bf885ad574ea9a2ab926d27421d11801fb907
Samurai2303 <meleschenko.andriy@gmail.com>
2022-08-23 13:53:30 +0300

    need to finish kminV lesson 7 and 8

--- kminV/Lesson 7/main.js
+++ kminV/Lesson 7/main.js
@@ -1,11 +1,136 @@
 // - Створити довільний елемент с id = text. використовуючи JavaScript, зробіть так, щоб при кліку на кнопку зникавл елемент с id="text".
+
+let text = document.createElement('p');
+text.setAttribute('id', 'text');
+text.innerText = 'lorem ipsum';
+document.body.appendChild(text);
+
+let button1 = document.createElement('button');
+button1.classList.add('button');
+button1.innerText = 'Show/Hide'
+document.body.appendChild(button1);
+button1.onclick = function () {
+    text.classList.toggle('text');
+}
+
 //     - Створіть кнопку, при кліку на яку, вона буде приховувати сама себе.
-// - Створити інпут який приймає вік людини та кнопку яка підтверджує дію.
-//     При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи менше він ніж 18, та повідомити про це користувача
+
+let button2 = document.createElement('button');
+document.body.appendChild(button2);
+button2.classList.add('button');
+button2.innerText = 'Hide me)';
+button2.onclick = function () {
+    button2.style.display = 'none';
+}
+
 // - Створіть меню, яке розгортається/згортається при клику
+
+let menu = document.createElement('div');
+document.body.appendChild(menu);
+menu.classList.add('menu');
+
+let arrow = document.createElement('i');
+arrow.classList.add('fa-solid', 'fa-arrow-down');
+menu.appendChild(arrow);
+
+let menuText = document.createElement('p');
+menuText.innerText = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi asperiores dignissimos dolor ducimus eveniet, id laboriosam nostrum quisquam soluta voluptatum. Ad animi aut consequatur cumque enim est harum magnam maiores maxime, perferendis porro quas ratione repellat sequi tempora tempore veritatis. Consequuntur corporis est excepturi expedita neque sint tempora totam ut!`;
+menuText.classList.add('menuText');
+menu.appendChild(menuText);
+
+arrow.onclick = function () {
+    menu.classList.toggle('showingText');
+};
+
 // - Створіть список коментарів , приклад об'єкту коментаря - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
+
+let commentsList = [
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+    {
+        title: 'lorem',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis est, maiores numquam praesentium quasi sapiente?'
+    },
+]
+
 // Вивести список коментарів в документ, кожний в своєму блоці.
 //     Додайте кожному коментарю по кнопці для згортання його body.
+
+let commentsDiv = document.createElement('div');
+commentsDiv.classList.add('commentsDiv');
+document.body.appendChild(commentsDiv);
+
+for (let item of commentsList) {
+
+    let commentDiv = document.createElement('div');
+    commentDiv.classList.add('commentDiv');
+    commentsDiv.appendChild(commentDiv);
+
+    let arrow1 = document.createElement('i');
+    arrow1.classList.add('fa-solid', 'fa-arrow-down', 'commentArrow');
+    commentDiv.appendChild(arrow1);
+
+    let commentTitle = document.createElement('h3');
+    commentTitle.innerText = item.title;
+    commentDiv.appendChild(commentTitle);
+
+    let p = document.createElement('p');
+    p.classList.add('menuText');
+    p.innerText = item.body;
+    commentDiv.appendChild(p);
+}
+
+let commentArrows = document.getElementsByClassName('commentArrow');
+let commentDivs = document.getElementsByClassName('commentDiv');
+
+for (let i = 0; i < commentArrows.length; i++) {
+    commentArrows[i].onclick = function () {
+        commentDivs[i].classList.toggle('showComment');
+    }
+}
+
 // - Створити 2 форми  по 2 інпути в кожній. створити кнопку при кліку на яку зчитується та виводиться на консоль інформація з цих 2х форм.
 //     Кнопка повинна лежати за межами форм (Щоб уникнути  перезавантаження сторінки)
 // Доступ до інпутів через Forms API. Отже дайте формі та інпутам всі необхідні атрибути.
